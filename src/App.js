4eaf7ed055551e9832f47dd20fd48ec969526498
Marcin Kos <marcin.kos.krk@gmail.com>
2022-03-12 18:10:34 +0100

    adding and deleting tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+
+function* uuidGen(){
+  let id = 0;
+
+  while (true) {
+    yield id;
+    id++;
+  }
+}
+
+const uuid = uuidGen()
 
 function App() {
+  const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleKeyUp = (event) => {
+    if (event.key === 'Enter'){
+      setTasks([...tasks, {
+        name: value,
+        id: uuid.next().value,
+        status: false
+      }]);
+      setValue('');
+      
+    }
+  }
+
+  function handleChangeStatus(id) {
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.status = !task.status
+      }
+      return task
+    })
+
+    setTasks(newTasks);
+  }
+
+  function handeDeleteTask(id) {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>todos</h1>
+      <input type="text"
+             value={value} 
+             onChange={handleChange}
+             onKeyUp={handleKeyUp}
+      />
+      <ul>
+        {tasks.map(({id, name, status}) => (
+          <li key={id} className='todo-item'>
+            <span className={status ? 'status done' : 'status active'}
+                  onClick={() => handleChangeStatus(id)}
+            />
+            {name}
+            <button onClick={() => handeDeleteTask(id)}>x</button>
+          </li>
+          ))}
+      </ul>
     </div>
   );
 }

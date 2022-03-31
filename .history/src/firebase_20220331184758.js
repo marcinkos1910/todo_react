9d7aec7f863993bca45a6a97e1fa17f9dae9ade0
Marcin Kos <marcin.kos.krk@gmail.com>
2022-03-31 19:12:11 +0200

    firebase set up

---
+++ .history/src/firebase_20220331184758.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqsezhnAkZam6rp0Sx4eWh7ETy3ak0Sg4",
+  authDomain: "todo-d02d3.firebaseapp.com",
+  projectId: "todo-d02d3",
+  storageBucket: "todo-d02d3.appspot.com",
+  messagingSenderId: "165389523424",
+  appId: "1:165389523424:web:978f416b3724c4db547385"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
